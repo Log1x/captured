7764c8446e8091998fc0dbf4e00b4d518491bada
Brandon <brandon@tendency.me>
2019-03-26 00:02:08 -0500

    Fix overlooked non-PSR-1 compliant statement

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ namespace Log1x\Captured;
 
 use Log1x\Captured\Captured;
 
-if (class_exists('Log1x\\Captured\\Captured') || !file_exists($composer = __DIR__ . '/vendor/autoload.php')) {
+if (class_exists('Log1x\\Captured\\Captured') || ! file_exists($composer = __DIR__ . '/vendor/autoload.php')) {
     return;
 }
 
